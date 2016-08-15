7ef5a7bf50c3b8427677a149d0e9e5c3d663ba95
MasSakrA <massakra@demnarctis.com>
2016-08-16 01:15:41 +0300

    Added event handler to buttons. Added comments.

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -7,36 +7,47 @@ import javafx.scene.control.*;
 import javafx.geometry.*;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-
+import java.util.HashMap;
 import java.io.*;
 
-// TODO create maven project
+// Главное javafx приложение
 public class Calculator extends Application {
-	
+	// Параметры калькулятора
 	protected String appName = "Javaculator";
 	protected int appWidth = 255;
 	protected int appHeight = 190;
+	// Поле ввода
 	protected TextField calcInput;	
+	// Два массива для храннения ссылок на кнопки (для цифровых кнопок и для кнопок операций)
+	protected HashMap<String, Button> inputButtons;
+	protected HashMap<String, Button> operationButtons;
 
 	public void start(Stage stage) {
-		
+		// Создаём основной слой приложения
 		GridPane rootPane = createRootPane();
+		// Добавляем элементы в шапку приложения
 		addHeader(rootPane);
-		
+		// Создаём слой с кнопками цифр
 		FlowPane inputButtonsPane = createInputButtonsPane(rootPane);
-		FlowPane operationButtonsPane = createOperationButtonsPane(rootPane);
-		rootPane.add(operationButtonsPane, 0, 2);
+		// Добавляем слой с цифрами в основной слой
 		rootPane.add(inputButtonsPane, 1, 2);
+		// Создаём слой с кнопками операций
+		FlowPane operationButtonsPane = createOperationButtonsPane(rootPane);
+		// Добавляем слой с операциями в основной слой
+		rootPane.add(operationButtonsPane, 0, 2);
 		
+		// Устанавливаем основной слой сцены и размер окна
 		Scene scene = new Scene(rootPane, appWidth, appHeight);
-		
+		// Заголовок приложения
 		stage.setTitle(appName);
-		stage.setResizable(false);		
+		// Запрещаем изменение размера окна
+		stage.setResizable(false);	
+		// Устанавливаем сцену окна	
 		stage.setScene(scene);
-		
+		// Отображаем окно
 		stage.show();
 	}
-	
+	// Для консоли
 	static public void main(String[] args) {
 		launch(args);
 	}
@@ -44,6 +55,7 @@ public class Calculator extends Application {
 	
 	/**
 	 * App main pane
+	 * Создаёт главный, корневой слой приложения
 	 */
 	public <T extends Pane> T createRootPane() {
 		GridPane rootPane = new GridPane();
@@ -59,17 +71,23 @@ public class Calculator extends Application {
 	
 	/**
 	 * Add header elements
+	 * Добавление некоторых элементов в шапку
 	 */
 	public void addHeader( GridPane pane ) {
+		// Текст в шапке
 		Text appHead = new Text(appName);
 		pane.add(appHead, 0, 0, 2, 1);
+		// Поле ввода
 		calcInput = new TextField();
+		// Его ширина
 		calcInput.setPrefWidth(530);
+		// Тобавляем в главный слой
 		pane.add(calcInput, 0, 1, 2, 1);
 	}
 	
 	/**
 	 * Section of buttons with input values
+	 * Создаёт кнопки с цифрами. Помещает их в отдельный слой
 	 */
 	public <T extends Pane, V extends Pane> T createInputButtonsPane( V rootPane ) {
 		String[] buttons = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."};
@@ -78,22 +96,41 @@ public class Calculator extends Application {
 	
 	/**
 	 * Section of buttons with operations for input values
+	 * Создаёт кнопки с операциями. Помещает их в отдельный слой
 	 */
 	public <T extends Pane, V extends Pane> T createOperationButtonsPane( V rootPane ) {
 		String[] buttons = {"+", "-", "*", "/", "%", "="};
 		return (T)createButtonsPane(buttons);
 	}
 	
+	/**
+	 * Общая функция для создания отдельного слоя и заполнения его кнопками
+	 */
 	public <T extends Pane> T createButtonsPane( String[] buttons ) {
+		// Новый слой
 		FlowPane pane = new FlowPane();
 		pane.setVgap(5);
 		pane.setHgap(5);
+		// Создаём кнопки в цикле, и добавляем обработчик событий.
 		for ( String buttonText : buttons ) {
 			Button button = new Button(buttonText);
+			button.setOnAction(new EventHandler<ActionEvent>() {
+				public void handle(ActionEvent e) {
+					// При клике просто добавляем значение в поле ввода
+					String currValue = calcInput.getText();
+					calcInput.setText(currValue + buttonText);
+				}
+			});
 			button.setPrefWidth(40);
 			pane.getChildren().add(button);
 		}
 		return (T)pane;
+	}
+	
+	// TODO Implemet
+	protected String findGroups( String input ) {
+		// Use regexp "/(\((?>[^)(]+|(?R))+\))/gx"
+		return "";
 	}
 	
 }
